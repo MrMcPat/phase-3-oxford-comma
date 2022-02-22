6ae39a3083b2760c6aac8b750356bbc7e4d53e0d
Patrick Liang <pl204511194@yahoo.com>
2022-02-22 10:29:21 -0500

    commit tree 8fb93987e8b32942e7d37476d1e2e3afcb55227e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    if array.length == 1
+    array.join
+    elsif array.length == 2
+        last_word = "and " + array.pop
+        array.push(last_word).join(" ")
 
+    elsif (array.length > 2)
+        last_word = "and " + array.pop
+        array.push(last_word).join(", ")
+    end
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
